a8761e7f4dc43957c947aa3a9d2d0f3c203f84b8
Ghislain Genay <ghislaingenay@gmail.com>
2022-09-02 20:14:02 +0700

    Add redis configuration to run docker-compose

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require("express")
 const redis = require('redis')
 
 const app = express()
-const client = redis.createClient() 
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+}) // Will be redirected to docker-compose file
+//Port: default number
+client.set("visits",  0)
 
 app.get("/", (req,res) => {
   client.get("visits", (err, visits) => {
     res.send("Number of visits is" + visits)
+    client.set("visits",Number(visits) + 1)
   })
 })
 
-app.listen(8081, () => {
-  console.log("Listening on port 8081")
+app.listen(4001, () => {
+  console.log("Listening on port 4001")
 })
